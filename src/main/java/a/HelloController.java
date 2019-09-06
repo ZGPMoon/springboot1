99b9efee40505583f25a075c758e5d54f7ea3549
eaglesinchina <eyeofeagle@126.com>
2019-09-06 12:16:38 +0800

    333

--- src/main/java/a/HelloController.java
+++ src/main/java/a/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
     @RequestMapping(value = "/a", method = {RequestMethod.GET})
     @ResponseBody
     public String successModel(HttpServletRequest request, HttpServletResponse reponse) {
-        return "success2";
+        return "success33333";
     }
 }
